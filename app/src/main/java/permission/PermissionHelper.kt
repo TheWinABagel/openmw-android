29833977379591161731e6b798ed4bf19e8a9b1a
Duron27 <duron27@gmail.com>
2024-07-11 11:06:16 -0400

    Update PermissionHelper.kt

--- app/src/main/java/permission/PermissionHelper.kt
+++ app/src/main/java/permission/PermissionHelper.kt
@@ -21,8 +21,12 @@ package permission
 
 import android.Manifest
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
+import android.widget.Toast
+import android.provider.Settings
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -30,14 +34,26 @@ object PermissionHelper {
     fun getWriteExternalStoragePermission(activity: Activity) {
         if (Build.VERSION.SDK_INT >= 23) {
             if (ContextCompat.checkSelfPermission(activity,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if (!ActivityCompat.shouldShowRequestPermissionRationale(activity,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                     ActivityCompat.requestPermissions(activity,
-                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 23
+                        (Manifest.permission.WRITE_EXTERNAL_STORAGE), 23
                     )
+                    } else {
+                        // If the user has denied the permission previously
+                        // Redirect the user to app settings
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", activity.packageName, null)
+                        intent.data = uri
+                        activity.startActivity(intent)
+                        Toast.makeText(activity, "Please enable storage permission in settings", Toast.LENGTH_LONG).show()
+                    }
+                    // wait till granted or freeze infinitelly
+                    while (ContextCompat.checkSelfPermission(activity,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                        Thread.sleep(50)
                 }
-            }
         }
     }
 }
